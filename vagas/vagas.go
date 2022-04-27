f347a58ee2f44548fddc5dbb8f3f074cba8d199f
Otavio Rocha <otaviorocha@outcenter.com.br>
2022-04-27 16:09:47 +0000

    trazendo carros na busca de vagas

--- vagas/vagas.go
+++ vagas/vagas.go
@@ -1,38 +1,36 @@
 package vagas
 
 import (
+	"database/sql"
 	"encoding/json"
 	"estacionamento-api/database"
-	"fmt"
 	"log"
 	"net/http"
 )
 
 var (
-	result 		[]Spot
-	id      	string
-	vehicle   	string
-	isempty   	bool
-	car 		*string
+	result 			[]Spot
+	id      		string
+	vehicle   		string
+	isempty   		bool
+	car 			Car
 )
 
 
 func GetSpot(w http.ResponseWriter, r *http.Request){
 	db := database.ConectDB()
 
-	rows, err := db.Query("SELECT * FROM spots")
+	rows, err := db.Query("SELECT s.id, s.vehicle , s.isempty, c.model, c.licenseplate FROM spots s LEFT OUTER JOIN cars c ON c.id  = s.car")
 	if err != nil {
-		fmt.Println("Erro no select")
-		// log.Fatal(err)
+		log.Fatal(err)
 	}
 	
 	for rows.Next() {
-		err := rows.Scan(&id, &vehicle, &isempty, &car)
+		err := rows.Scan(&id, &vehicle, &isempty, &car.Model, &car.LicensePlate)
 		if err != nil {
-			fmt.Println("Erro nas rows")
 			log.Fatal(err)
 		}
-		result = append(result, Spot{ID: id, Vehicle: vehicle, IsEmpty: isempty})
+		result = append(result, Spot{ID: id, Vehicle: vehicle, IsEmpty: isempty, Car: &Car{LicensePlate:car.LicensePlate, Model: car.Model} })
 	}
 	err = rows.Err()
 	if err != nil {
@@ -51,6 +49,6 @@ type Spot struct {
 }
 
 type Car struct {
-	LicensePlate	string		`json:"licenseplate,omitempty"`
-	Model			string 		`json:"model,omitempty"`
+	LicensePlate	sql.NullString		`json:"licenseplate,omitempty"`
+	Model			sql.NullString 		`json:"model,omitempty"`
 }
